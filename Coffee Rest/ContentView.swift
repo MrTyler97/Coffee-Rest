fb4a36e507a702e615f980ea264b4373c68fbc17
Victor Tyler <146302984+MrTyler97@users.noreply.github.com>
2025-09-09 15:05:12 -0400

    Adding sleep counter using Stepper

--- Coffee Rest/ContentView.swift
+++ Coffee Rest/ContentView.swift
@@ -8,14 +8,12 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var sleepAmount = 8.0
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
-        }
-        .padding()
+        // Stepper adds a +/- counter to a variable to change. From range of 4 to 12 hours
+        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
     }
 }
 
