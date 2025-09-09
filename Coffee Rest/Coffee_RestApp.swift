499c75f23369ffc51f4e1897eae59f91a0fd5854
Victor Tyler <146302984+MrTyler97@users.noreply.github.com>
2025-09-09 14:50:32 -0400

    Initial Commit

---
+++ Coffee Rest/Coffee_RestApp.swift
@@ -0,0 +1,17 @@
+//
+//  Coffee_RestApp.swift
+//  Coffee Rest
+//
+//  Created by Vic  on 9/9/25.
+//
+
+import SwiftUI
+
+@main
+struct Coffee_RestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
