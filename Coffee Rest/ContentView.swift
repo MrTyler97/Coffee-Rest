c2f7539a2b4304076879a68c6f9d6c07b587ecfe
Victor Tyler <146302984+MrTyler97@users.noreply.github.com>
2025-09-11 15:46:54 -0400

    Adding ML model via CoreML

--- Coffee Rest/ContentView.swift
+++ Coffee Rest/ContentView.swift
@@ -6,22 +6,39 @@
 //
 
 import SwiftUI
+import CoreML
 
 struct ContentView: View {
     
     @State var sleepAmount = 8.0
     @State var wakeUp = Date.now
     @State var coffeIntake = 1
-    
-    // Use DateComponets when you want to extract specific information.
-    func exampleDates() -> DateComponents {
-        // Extract only hour and minute
-        let components = Calendar.current.dateComponents([.hour, .minute], from: .now)
-        return components
-    }
+    @State var alertTitle = ""
+    @State var alertMessage = ""
+    @State var alertShowing = false
     
     func calculateSleep(){
+        // Catch potenial errors in loading/running model
+        do{
+            // Create the model with appropriate configurations
+            let model = try SleepPredictor(configuration: MLModelConfiguration())
+            // Extract only hour and minutes data
+            let components = Calendar.current.dateComponents([.hour, .minute], from: wakeUp)
+            // Convert hour and minutes to seconds from midnight (today)
+            let hours = (components.hour ?? 0) * 60 * 60
+            let minutes = (components.minute ?? 0) * 60
+            // Get prediction model needs 3 input params desired wake up time (in seconds), desired sleep time (in hours), and number of coffe cups consumed.
+            let prediction = try model.prediction(wake: Double(hours + minutes), estimatedSleep: sleepAmount , coffee: Double(coffeIntake))
+            // Subtract desired wake up time from predicted sleep
+            let sleepTime = wakeUp - prediction.actualSleep
+            alertTitle = "Your preferred bed time is... "
+            alertMessage = sleepTime.formatted(date: .omitted, time: .shortened)
+        } catch{
+            alertTitle = "Error"
+            alertMessage = "There was a problem getting your prediction"
+        }
         
+        alertShowing = true
     }
     
     
@@ -47,10 +64,14 @@ struct ContentView: View {
                     .padding()
                 
             }
-            .navigationTitle("CoffeeRest")
+            .navigationTitle("Coffee Rest")
             .padding()
-            Button("Calculate", action: calculateSleep)
-            
+            .alert(alertTitle, isPresented: $alertShowing){
+                Button("Ok") {}
+            } message: {
+                Text(alertMessage)
+            }
+            Button("Ask AI", action: calculateSleep)
         }
     }
 }
