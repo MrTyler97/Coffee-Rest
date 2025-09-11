070c0e116745305b6cf9dde1fcf669f04cbffa30
Victor Tyler <146302984+MrTyler97@users.noreply.github.com>
2025-09-11 14:05:17 -0400

    Adding UI elements

--- Coffee Rest/ContentView.swift
+++ Coffee Rest/ContentView.swift
@@ -11,16 +11,47 @@ struct ContentView: View {
     
     @State var sleepAmount = 8.0
     @State var wakeUp = Date.now
+    @State var coffeIntake = 1
+    
+    // Use DateComponets when you want to extract specific information.
+    func exampleDates() -> DateComponents {
+        // Extract only hour and minute
+        let components = Calendar.current.dateComponents([.hour, .minute], from: .now)
+        return components
+    }
+    
+    func calculateSleep(){
+        
+    }
+    
     
     var body: some View {
-        // Stepper adds a +/- counter to a variable to change. From range of 4 to 12 hours
-        Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+        NavigationStack{
+            VStack{
+                Text("When do you want to wake up?")
+                    .font(.headline)
+                // Date picker allows us to choose dates. Can only use Date object not DateComponents
+                DatePicker("Select a wake up time" , selection: $wakeUp, displayedComponents:  .hourAndMinute)
+                    .labelsHidden()
+                    .padding()
+                
+                Text("Desired amount of sleep?")
+                    .font(.headline)
+                // Stepper adds a +/- counter to a variable to change. From range of 4 to 12 hours
+                Stepper("\(sleepAmount.formatted()) hours", value: $sleepAmount, in: 4...12, step: 0.25)
+                    .padding()
+                
+                Text("How much coffee did you intake?")
+                    .font(.headline)
+                Stepper("\(coffeIntake) cups", value: $coffeIntake, in: 1...10)
+                    .padding()
+                
+            }
+            .navigationTitle("CoffeeRest")
             .padding()
-        // Date picker allows us to choose dates
-        DatePicker("Select a wake up time" , selection: $wakeUp, in:
-            //Range from now to end of day
-            Date.now...Date.now.addingTimeInterval(86400), displayedComponents:  .hourAndMinute)
-            .labelsHidden()
+            Button("Calculate", action: calculateSleep)
+            
+        }
     }
 }
 
